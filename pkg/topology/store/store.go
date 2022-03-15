34a89ad812ff1c41f20b18554670de850634b3a1
LandryBe <lbenguigui@gmail.com>
2022-03-15 10:10:13 +0100

    Fix git remote when we restart the agent and update the token.

--- pkg/topology/store/store.go
+++ pkg/topology/store/store.go
@@ -10,6 +10,7 @@ import (
 	"github.com/ldez/go-git-cmd-wrapper/v2/clone"
 	"github.com/ldez/go-git-cmd-wrapper/v2/config"
 	"github.com/ldez/go-git-cmd-wrapper/v2/git"
+	"github.com/ldez/go-git-cmd-wrapper/v2/remote"
 	"github.com/ldez/go-git-cmd-wrapper/v2/types"
 	"github.com/rs/zerolog/log"
 	"github.com/traefik/neo-agent/pkg/platform"
@@ -71,6 +72,10 @@ func (s *Store) cloneRepository(ctx context.Context) error {
 		if !strings.Contains(output, "already exists and is not an empty directory") {
 			return fmt.Errorf("create local repository: %w %s", err, output)
 		}
+
+		if err = s.updateOriginURL(); err != nil {
+			return fmt.Errorf("update origin remote URL %w", err)
+		}
 	}
 
 	output, err = git.Config(config.Local, config.Add("user.email", "hubagent@traefik.io"), git.CmdExecutor(s.gitExecutor))
@@ -81,6 +86,24 @@ func (s *Store) cloneRepository(ctx context.Context) error {
 	output, err = git.Config(config.Local, config.Add("user.name", "Hub Agent"), git.CmdExecutor(s.gitExecutor))
 	if err != nil {
 		return fmt.Errorf("%w: %s", err, output)
+	}
+
+	return nil
+}
+
+func (s *Store) updateOriginURL() error {
+	url, err := git.Remote(remote.GetURL("origin"), git.CmdExecutor(s.gitExecutor))
+	if err != nil {
+		return fmt.Errorf("get remote URL %w", err)
+	}
+
+	if url != s.gitRepo {
+		// This case happens when the agent restart and the token is updated.
+		// In this case, we need to update the URL of the origin remote.
+		_, err = git.Remote(remote.SetURL("origin", s.gitRepo, ""), git.CmdExecutor(s.gitExecutor))
+		if err != nil {
+			return fmt.Errorf("set remote URL %w", err)
+		}
 	}
 
 	return nil
