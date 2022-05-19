cc3a8e277da58794c593b4f55f2aabfad7599490
Julien Salleyron <julien.salleyron@gmail.com>
2022-05-19 15:00:13 +0200

    Remove providers in router metrics and remove service metrics

--- pkg/metrics/parser.go
+++ pkg/metrics/parser.go
@@ -1,6 +1,8 @@
 package metrics
 
 import (
+	"strings"
+
 	dto "github.com/prometheus/client_model/go"
 )
 
@@ -25,12 +27,6 @@ func (p TraefikParser) Parse(m *dto.MetricFamily) []Metric {
 	var metrics []Metric
 
 	switch *m.Name {
-	case "traefik_service_request_duration_seconds":
-		metrics = append(metrics, p.parseServiceRequestDuration(m.Metric)...)
-
-	case "traefik_service_requests_total":
-		metrics = append(metrics, p.parseServiceRequestTotal(m.Metric)...)
-
 	case "traefik_router_request_duration_seconds":
 		metrics = append(metrics, p.parseRouterRequestDuration(m.Metric)...)
 
@@ -39,64 +35,6 @@ func (p TraefikParser) Parse(m *dto.MetricFamily) []Metric {
 	}
 
 	return metrics
-}
-
-func (p TraefikParser) parseServiceRequestDuration(metrics []*dto.Metric) []Metric {
-	var enrichedMetrics []Metric
-
-	for _, metric := range metrics {
-		hist := HistogramFromMetric(metric)
-		if hist == nil {
-			continue
-		}
-
-		// Service metrics doesn't hold information about the ingress it went through.
-		svc := p.guessService(metric.Label)
-		if svc == "" {
-			continue
-		}
-		hist.Name = MetricRequestDuration
-		hist.Service = svc
-
-		enrichedMetrics = append(enrichedMetrics, hist)
-	}
-
-	return enrichedMetrics
-}
-
-func (p TraefikParser) parseServiceRequestTotal(metrics []*dto.Metric) []Metric {
-	var enrichedMetrics []Metric
-
-	for _, metric := range metrics {
-		counter := CounterFromMetric(metric)
-		if counter == 0 {
-			continue
-		}
-
-		svc := p.guessService(metric.Label)
-		if svc == "" {
-			continue
-		}
-
-		// Service metrics doesn't hold information about the ingress it went through.
-		enrichedMetrics = append(enrichedMetrics, &Counter{
-			Name:    MetricRequests,
-			Service: svc,
-			Value:   counter,
-		})
-
-		metricErrorName := getMetricErrorName(metric.Label, "code")
-		if metricErrorName == "" {
-			continue
-		}
-		enrichedMetrics = append(enrichedMetrics, &Counter{
-			Name:    metricErrorName,
-			Service: svc,
-			Value:   counter,
-		})
-	}
-
-	return enrichedMetrics
 }
 
 func (p TraefikParser) parseRouterRequestDuration(metrics []*dto.Metric) []Metric {
@@ -108,15 +46,15 @@ func (p TraefikParser) parseRouterRequestDuration(metrics []*dto.Metric) []Metri
 			continue
 		}
 
-		ingress := p.guessIngress(metric.Label)
-		if ingress == "" {
+		router := p.guessRouter(metric.Label)
+		if router == "" {
 			continue
 		}
 
 		// Service can't be accurately obtained on router metrics. The service label holds the service name to which the
 		// router will deliver the traffic, not the leaf node of the service tree (e.g. load-balancer, wrr).
 		hist.Name = MetricRequestDuration
-		hist.Ingress = ingress
+		hist.Ingress = router
 
 		enrichedMetrics = append(enrichedMetrics, hist)
 	}
@@ -133,8 +71,8 @@ func (p TraefikParser) parseRouterRequestTotal(metrics []*dto.Metric) []Metric {
 			continue
 		}
 
-		ingress := p.guessIngress(metric.Label)
-		if ingress == "" {
+		router := p.guessRouter(metric.Label)
+		if router == "" {
 			continue
 		}
 
@@ -142,7 +80,7 @@ func (p TraefikParser) parseRouterRequestTotal(metrics []*dto.Metric) []Metric {
 		// router will deliver the traffic, not the leaf node of the service tree (e.g. load-balancer, wrr).
 		enrichedMetrics = append(enrichedMetrics, &Counter{
 			Name:    MetricRequests,
-			Ingress: ingress,
+			Ingress: router,
 			Value:   counter,
 		})
 
@@ -152,7 +90,7 @@ func (p TraefikParser) parseRouterRequestTotal(metrics []*dto.Metric) []Metric {
 		}
 		enrichedMetrics = append(enrichedMetrics, &Counter{
 			Name:    metricErrorName,
-			Ingress: ingress,
+			Ingress: router,
 			Value:   counter,
 		})
 	}
@@ -160,12 +98,15 @@ func (p TraefikParser) parseRouterRequestTotal(metrics []*dto.Metric) []Metric {
 	return enrichedMetrics
 }
 
-func (p TraefikParser) guessService(lbls []*dto.LabelPair) string {
-	return getLabel(lbls, "service")
-}
+func (p TraefikParser) guessRouter(lbls []*dto.LabelPair) string {
+	router := getLabel(lbls, "router")
 
-func (p TraefikParser) guessIngress(lbls []*dto.LabelPair) (ingress string) {
-	return getLabel(lbls, "router")
+	if !strings.HasSuffix(router, "@hub") {
+		return ""
+	}
+
+	// Remove @hub suffix before returning
+	return router[:len(router)-4]
 }
 
 func getMetricErrorName(lbls []*dto.LabelPair, statusName string) string {
--- pkg/metrics/scraper_test.go
+++ pkg/metrics/scraper_test.go
@@ -51,23 +51,12 @@ func TestScraper_ScrapeTraefik(t *testing.T) {
 	got, err := s.Scrape(context.Background())
 	require.NoError(t, err)
 
-	// service
-	assert.Contains(t, got, &metrics.Histogram{Name: metrics.MetricRequestDuration, Service: "default-whoami-80@docker", Sum: 0.021072671000000005, Count: 12})
-	assert.Contains(t, got, &metrics.Counter{Name: metrics.MetricRequests, Service: "default-whoami-sdfsdfsdsd@docker", Value: 12})
-	assert.Contains(t, got, &metrics.Counter{Name: metrics.MetricRequests, Service: "default-whoami-80@docker", Value: 14})
-	assert.Contains(t, got, &metrics.Counter{Name: metrics.MetricRequestClientErrors, Service: "default-whoami-80@docker", Value: 14})
-	assert.Contains(t, got, &metrics.Counter{Name: metrics.MetricRequests, Service: "default-whoami2-80@docker", Value: 16})
-	assert.Contains(t, got, &metrics.Counter{Name: metrics.MetricRequestErrors, Service: "default-whoami2-80@docker", Value: 16})
-	assert.Contains(t, got, &metrics.Counter{Name: metrics.MetricRequests, Service: "default-whoami3-80@docker", Value: 15})
-	assert.Contains(t, got, &metrics.Counter{Name: metrics.MetricRequestErrors, Service: "default-whoami3-80@docker", Value: 15})
-
 	// router
-	assert.Contains(t, got, &metrics.Histogram{Name: metrics.MetricRequestDuration, Ingress: "myIngress-default-example-com@docker", Sum: 0.0137623, Count: 1})
-	assert.Contains(t, got, &metrics.Histogram{Name: metrics.MetricRequestDuration, Ingress: "default-myIngressRoute-6f97418635c7e18853da@docker", Sum: 0.0216373, Count: 1})
-	assert.Contains(t, got, &metrics.Counter{Name: metrics.MetricRequests, Ingress: "myIngress-default-example-com@docker", Value: 2})
-	assert.Contains(t, got, &metrics.Counter{Name: metrics.MetricRequests, Ingress: "default-myIngressRoute-6f97418635c7e18853da@docker", Value: 1})
-	assert.Contains(t, got, &metrics.Counter{Name: metrics.MetricRequests, Service: "default-myIngressRoute-6f97418635c7e18853da@docker", Value: 17})
-	assert.Contains(t, got, &metrics.Counter{Name: metrics.MetricRequestErrors, Service: "default-myIngressRoute-6f97418635c7e18853da@docker", Value: 17})
-
-	require.Len(t, got, 14)
+	assert.Contains(t, got, &metrics.Histogram{Name: metrics.MetricRequestDuration, Ingress: "myIngress-default-example-com", Sum: 0.0137623, Count: 1})
+	assert.Contains(t, got, &metrics.Histogram{Name: metrics.MetricRequestDuration, Ingress: "default-myIngressRoute-6f97418635c7e18853da", Sum: 0.0216373, Count: 1})
+	assert.Contains(t, got, &metrics.Counter{Name: metrics.MetricRequests, Ingress: "myIngress-default-example-com", Value: 2})
+	assert.Contains(t, got, &metrics.Counter{Name: metrics.MetricRequestClientErrors, Ingress: "myIngress-default-example-com", Value: 4})
+	assert.Contains(t, got, &metrics.Counter{Name: metrics.MetricRequestErrors, Ingress: "myIngress-default-example-com", Value: 6})
+	assert.Contains(t, got, &metrics.Counter{Name: metrics.MetricRequests, Ingress: "default-myIngressRoute-6f97418635c7e18853da", Value: 1})
+	require.Len(t, got, 8)
 }
